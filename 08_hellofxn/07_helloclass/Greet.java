2d1abf9a334d1bc4239fcbb26e83876df306c693
Kevin Cheng <kcheng30@stuy.edu>
2021-09-30 17:48:58 -0400

    greeting

--- 08_hellofxn/07_helloclass/Greet.java
+++ 08_hellofxn/07_helloclass/Greet.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Greet {
+	public static void greet() {
+		String name;
+		Scanner Sname = new Scanner(System.in);
+		System.out.println("What is your name?");
+		name = Sname.nextString();
+
+		System.out.print("Greetings, ");
+		System.out.println(name);
+	}
 	public static void main(String[] args) {
-		System.out.println("No hablo queso");
+		greet();
 	}
 }
