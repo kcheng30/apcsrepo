bd33473f75dcfc07cd07098b09ff68142278d8c1
Kevin Cheng <kcheng30@stuy.edu>
2021-10-04 12:46:47 -0400

    other file method

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,15 @@
+//*
+Kevin Cheng - Frog Hat
+APCS
+HW09 -- What Are BigSibs Good For?
+2021-10-04
+
+DISCOVERIES
+You can call a  method from a separate file by saying the file the method is in, followed by a period and the name of the method.
+
+UNRESOLVED QUESTIONS
+*/
+
 public class BigSib {
 	public static void greet (String s) {
 		System.out.println("Greetings, " + s);
