edfd89fcc38eb15ae4645329204e1f6229155019
Kevin Cheng <90802754+kcheng30@users.noreply.github.com>
2021-10-04 17:14:34 -0400

    teams and duckies

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,5 +1,5 @@
 //*
-Frog Hats - Oscar Breen, Diana Akhmedova + Narwhal, Kevin Cheng + Frog Hat
+Frog Hats: Oscar Breen, Diana Akhmedova + Narwhal, Kevin Cheng + Frog Hat
 APCS
 HW10 - Refactor Big Sib One
 2021-10-04
