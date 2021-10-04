2275cbf47b9454727c22afdfded85ff8be44fc13
Kevin Cheng <kcheng30@stuy.edu>
2021-10-04 17:09:22 -0400

    non-void

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,16 @@
+//*
+Kevin Cheng
+APCS
+HW10 - Refactor Big Sib One
+2021-10-04
+
+DISCOVERIES
+
+UNSOLVED QUESTIONS
+*/
+
+public class BigSib {
+	public static String greet(String s) {
+		return "Greetings, " + s;
+	}
+}
