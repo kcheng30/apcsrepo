7edd5a026d984061a6f37905f79713a0513de5b4
Kevin Cheng <kcheng30@stuy.edu>
2021-10-06 17:18:07 -0400

    constructor

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,3 +1,20 @@
+/*
+Kevin Cheng
+APCS
+HW13 - Where do BigSibs Come From?
+2021-10-06
+
+DISCO
+What a constructor is
+You can have things work without labeling them as static
+
+QCC
+What are the alternatives to static, and what do all of them do?
+What purposes do constructors serve?
+Why can't they have a return value?
+Why must they be public?
+*/
+
 public class BigSib {
 	
 	String helloMsg;
