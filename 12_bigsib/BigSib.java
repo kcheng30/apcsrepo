157bbe4dd1618fa5a94d6807ccf4f220888fcb05
Kevin Cheng <kcheng30@stuy.edu>
2021-10-05 23:08:15 -0400

    instance variable

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,3 +1,17 @@
+//*
+Kevin Cheng
+APCS
+HW12 - On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+
+DISCO
+Variables can also have a static descriptor.
+
+QCC
+What does BigSib <name> = new BigSib(); do?
+Why does it matter that the instance variable is static?
+*/
+
 public class BigSib {
 
 	static String helloMsg;
