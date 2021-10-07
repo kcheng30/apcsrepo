86023a54517f267f3d5c1321b1ffefb2159df259
Kevin Cheng <kcheng30@stuy.edu>
2021-10-07 13:59:39 -0400

    class notes

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -16,8 +16,8 @@ public class BigSib {
 
 	static String helloMsg;
 
-	public static String setHelloMsg(String s) {
-		return helloMsg = s;
+	public void setHelloMsg() {
+		 helloMsg = "Word up";
 	}
 
 	public static String greet(String s) {
