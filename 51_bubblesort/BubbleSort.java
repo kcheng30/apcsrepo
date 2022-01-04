26b1084b98e2239ed5013d5a3aa6728aab9c272e
Kevin Cheng <kcheng30@stuy.edu>
2022-01-03 22:30:23 -0500

    skelly + bubble sort

--- 51_bubblesort/BubbleSort.java
+++ 51_bubblesort/BubbleSort.java
@@ -1,44 +1,136 @@
-/*
- * Team Frog Hats - Jacob Ng, Kevin Cheng, Hamim Seam
- * APCS
- * HW51 - Dat Bubbly Tho
- * 2021-1-3
- *
- * DISCO:
- * For an array of length n, you need at most n-1 passes to fully sort it with Bubble Sort.
- * The "bubble" of sorted nums becomes 1 bigger for every completed pass.
- * This means that the mth pass requires only n-m comparisons. 
- *
- * QCC:
- * How should we have the program stop when the passes become redundant?
- * How can we implement the "mth pass requires n-m comparisons"?
- */
+// Clyde "Thluffy" Sinclair
+// APCS pd0
+// HW51 -- implementing bubblesort
+// 2022-01-04t
+// time spent:  hrs
 
-public class BubbleSort {
-	
-	public static void sort( int[] array ) {
-		for ( int i = 0; i < array.length - 1; i++ ) {
-			for ( int j = array.length - 1; j > 0; j-- ) {
-				if ( array[j] < array[j - 1] ) {
-					int temp = array[j];
-					array[j] = array[j - 1];
-					array[j - 1] = temp;
+/******************************
+ * class BubbleSort -- implements bubblesort algorithm (vanilla)
+ *
+ * ALGO:
+ * 
+ * DISCO
+ *
+ * QCC
+ * q0: If a pass requires no swaps, what do you know?
+ * a0: The array is already sorted
+ * q1: After pass p, what do you know?
+ * a1: p elements are guaranteed to be sorted in the array, starting from the left
+ * q2: How many passes are necessary to completely sort?
+ * a2: 1 less than the length of the array being sorted
+ * 
+ ******************************/
+
+import java.util.ArrayList;
+
+public class BubbleSort
+{
+
+  //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+  //precond:  lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi ) {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+      //     offset + rand int on interval [lo,hi]
+      retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+      size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al )
+  {
+    int randomIndex;
+    //setup for traversal fr right to left
+    for( int i = al.size()-1; i > 0; i-- ) {
+      //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+      //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+  // VOID version of bubbleSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void bubbleSortV( ArrayList<Comparable> data )
+  {
+		for ( int i = 0; i < data.size() - 1; i++ ) {
+			for ( int j = data.size() - 1; j > 0; j-- ) {
+				if ( data.get(j).compareTo( data.get(j - 1) ) < 0 ) {
+					Comparable temp = data.get(j);
+					data.set( j, data.get(j - 1) );
+					data.set( j - 1, temp );
 				}
 			}
 		}
-	}
+  }
 
-	public static String arrayString( int[] array ) {
-		String arrayS = "[ ";
-		for ( int num : array )
-			arrayS += num + ", ";
-		arrayS += "]";
-		return arrayS;
-	}
+  // ArrayList-returning bubbleSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input )
+  {
+	ArrayList<Comparable> retArray = new ArrayList<Comparable>();
+		for ( int i = 0; i < input.size(); i++ ) {
+			for ( int j = input.size() - 1; j > 0; j-- ) {
+				if ( input.get(j).compareTo( input.get(j - 1) ) < 0 ) {
+					Comparable temp = input.get(j);
+					input.set( j, input.get(j - 1) );
+					input.set( j - 1, temp );
+				}
+			}
+			retArray.add( input.get(i) );
+		}
+	return retArray;
+  }
 
-	public static void main( String[] args ) {
-		int[] sample1 = { 5, 4, 3, 2, 1 };
-		sort(sample1);
-		System.out.println(arrayString(sample1));
-	}
-}
+
+  public static void main( String [] args )
+  {
+
+    /*===============for VOID methods=============
+      ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "ArrayList glen before sorting:\n" + glen );
+      bubbleSortV(glen);
+      System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "ArrayList coco before sorting:\n" + coco );
+      bubbleSortV(coco);
+      System.out.println( "ArrayList coco after sorting:\n" + coco );
+      ============================================*/
+
+      ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "ArrayList glen before sorting:\n" + glen );
+      ArrayList glenSorted = bubbleSort( glen );
+      System.out.println( "sorted version of ArrayList glen:\n" 
+      + glenSorted );
+      System.out.println( "ArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "ArrayList coco before sorting:\n" + coco );
+      ArrayList cocoSorted = bubbleSort( coco );
+      System.out.println( "sorted version of ArrayList coco:\n" 
+      + cocoSorted );
+      System.out.println( "ArrayList coco after sorting:\n" + coco );
+      System.out.println( coco );
+
+  }//end main
+
+}//end class BubbleSort
